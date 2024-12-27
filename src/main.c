bdbbf1972cf1e3ec9cb6828c6fe8b1fd8079c533
hocine dev <hocinedev4@gmail.com>
2024-12-27 14:26:05 +0100

    Ajout  main.c

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("démarage de programe\n");
+    
+    // TODO: Ajouter les appels pour le client et le serveur ici
+
+    return 0;
+}
